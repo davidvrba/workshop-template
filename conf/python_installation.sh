008c9bdcf2596bcd4349cdddd86df2a1143bfe65
vrbad <david.vrba@emplifi.io>
2024-10-06 09:56:22 +0200

    Update for Spark 3.5.3

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -8,7 +8,7 @@ pip3 install py4j --break-system-packages
 apt --assume-yes remove python3-jsonschema
 pip3 install jsonschema --break-system-packages
 pip3 install jupyter --break-system-packages
+pip3 install delta-spark==3.2.1 --break-system-packages
 
-
-wget /home/ubuntu https://archive.apache.org/dist/spark/spark-3.5.1/spark-3.5.1-bin-hadoop3.tgz
-tar xvf spark-3.5.1-bin-hadoop3.tgz
+wget /home/ubuntu https://archive.apache.org/dist/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
+tar xvf spark-3.5.3-bin-hadoop3.tgz
